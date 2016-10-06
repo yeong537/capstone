35b2dc335047cdec9335f276476aa3c8a4eb8cb9
yeong537 <yeong735@gmail.com>
2016-10-06 17:17:41 +0900

    image road

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -75,6 +75,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "image";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox2
             // 
--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -16,6 +16,26 @@ namespace WindowsFormsApplication3
         {
             InitializeComponent();
         }
-        
+
+        private System.Drawing.Bitmap pic1;
+        private string name1;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = "jpg";
+            openFile.Filter = "Graphics interchange Format (*.jpg)|*.jpg|All files(*.*)|*.*";
+            openFile.ShowDialog();
+
+            if (openFile.FileName.Length > 0)
+
+            {
+                pic1 = new Bitmap(openFile.FileName);
+                name1 = openFile.FileName;
+                
+                pictureBox1.Image = pic1;
+
+            }
+        }
     }
 }
